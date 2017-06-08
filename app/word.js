e2b077dc414bbcfe4f9dda8b0e9a7d67ffd1695b
omobolajimary <omobolajimary@gmail.com>
2017-06-08 17:41:53 +0100

    Add word.js file

--- app/word.js
+++ app/word.js
@@ -1,6 +1,19 @@
 'use strict'
 module.export = {
-    word:(str)=>{
-        
+    words = (word_string)=> {
+    word_count_json = {};
+    words_array = word_string.split(' ');
+  let i = 0;
+        while (i < words_array.length){
+            if (typeof word_count_json[words_array[i]] ==='undefined'){
+                word_count_json[words_array[i]] = 1;
+            }
+            else {
+                word_count_json[words_array[i]]+=1;
+             }
+         i++ ;
     }
+    return word_count_json;
 }
+}
+        
